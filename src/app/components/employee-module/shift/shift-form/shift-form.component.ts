0b9b1bd111851381dab63442fe79e4a5cf86fb32
pham bao <baopt>
2022-08-15 11:29:49 +0700

    update future shift

--- src/app/components/employee-module/shift/shift-form/shift-form.component.ts
+++ src/app/components/employee-module/shift/shift-form/shift-form.component.ts
@@ -1,4 +1,12 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnChanges,
+    OnInit,
+    Output,
+    SimpleChanges,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { MessageService } from 'primeng/api';
@@ -6,146 +14,165 @@ import AppConstant from 'src/app/utilities/app-constants';
 import AppData from 'src/app/utilities/app-data';
 import AppUtil from 'src/app/utilities/app-util';
 import { SymbolService } from 'src/app/service/symbol.service';
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-shift-form',
-  templateUrl: './shift-form.component.html',
-  styles: [
-      `
-          :host ::ng-deep {
-              #phonePrefix .p-dropdown {
-                  width: 93px;
-              }
-          }
-      `,
-  ],
+    selector: 'app-shift-form',
+    templateUrl: './shift-form.component.html',
+    styles: [
+        `
+            :host ::ng-deep {
+                #phonePrefix .p-dropdown {
+                    width: 93px;
+                }
+                .p-calendar {
+                    width: 100%;
+                }
+            }
+        `,
+    ],
 })
 export class ShiftFormComponent implements OnInit, OnChanges {
-  public appConstant = AppConstant;
-  @Input('formData') formData: any = {};
-  @Input('isReset') isReset: boolean = false;
-  @Input('isEdit') isEdit: boolean = false;
-  @Input('display') display: boolean = false;
-  @Output() onCancel = new EventEmitter();
-  title: string = '';
+    public appConstant = AppConstant;
+    @Input('formData') formData: any = {};
+    @Input('isReset') isReset: boolean = false;
+    @Input('isEdit') isEdit: boolean = false;
+    @Input('display') display: boolean = false;
+    @Output() onCancel = new EventEmitter();
+    title: string = '';
 
-  SymbolForm: FormGroup = new FormGroup({});
+    SymbolForm: FormGroup = new FormGroup({});
 
-  optionCountries = AppData.COUNTRIES;
-  countryCodes: any[] = [];
+    optionCountries = AppData.COUNTRIES;
+    countryCodes: any[] = [];
 
-  isSubmitted = false;
-  isInvalidForm = false;
-  failPassword: boolean = false;
+    isSubmitted = false;
+    isInvalidForm = false;
+    failPassword: boolean = false;
 
-  constructor(
-      private fb: FormBuilder,
-      private translateService: TranslateService,
-      private messageService: MessageService,
-      private SymbolService: SymbolService
-  ) {
-      this.SymbolForm = this.fb.group(
-          {
-              id: [''],
-              code: ['', [Validators.required]],
-              name: ['', [Validators.required]]
-          }
-      );
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-      if (
-          this.isEdit &&
-          this.formData &&
-          Object.keys(this.formData).length > 0
-      ) {
-          this.title = AppUtil.translate(this.translateService, 'label.edit_Symbol');
-          this.SymbolForm.setValue({
-              id: this.formData.id,
-              code: this.formData.code,
-              name: this.formData.name,
-          });
-      } else {
-          this.title = AppUtil.translate(this.translateService, 'label.add_Symbol');
-      }
-  }
+    constructor(
+        private fb: FormBuilder,
+        private translateService: TranslateService,
+        private messageService: MessageService,
+        private SymbolService: SymbolService
+    ) {
+        this.SymbolForm = this.fb.group({
+            id: [''],
+            code: ['', [Validators.required]],
+            name: ['', [Validators.required]],
+            timeIn: [''],
+            timeOut: [''],
+            timeTotal: [''],
+            note: [''],
+            status: [true],
+        });
+    }
+    ngOnChanges(changes: SimpleChanges): void {
+        if (
+            this.isEdit &&
+            this.formData &&
+            Object.keys(this.formData).length > 0
+        ) {
+            this.title = AppUtil.translate(
+                this.translateService,
+                'label.shift_update'
+            );
+            this.SymbolForm.setValue({
+                id: this.formData.id,
+                code: this.formData.code,
+                name: this.formData.name,
+                timeIn: moment(this.formData.timeIn).format(
+                    this.appConstant.FORMAT_DATE.T_DATE
+                ),
+                timeOut: moment(this.formData.timeOut).format(
+                    this.appConstant.FORMAT_DATE.T_DATE
+                ),
+                timeTotal: this.formData.timeTotal,
+                note: this.formData.note,
+                status: this.formData.status,
+            });
+        } else {
+            this.title = AppUtil.translate(
+                this.translateService,
+                'label.shift_add'
+            );
+        }
+    }
 
-  onReset() {
-      this.isInvalidForm = false;
-      this.SymbolForm.reset();
-  }
+    onReset() {
+        this.isInvalidForm = false;
+        this.SymbolForm.reset();
+    }
 
-  ngOnInit() {
-      this.countryCodes = AppUtil.getCountries();
-  }
+    ngOnInit() {}
 
-  checkValidValidator(fieldName: string) {
-      return ((this.SymbolForm.controls[fieldName].dirty ||
-          this.SymbolForm.controls[fieldName].touched) &&
-          this.SymbolForm.controls[fieldName].invalid) ||
-          (this.isInvalidForm &&
-              this.SymbolForm.controls[fieldName].invalid)
-          ? 'ng-invalid ng-dirty'
-          : '';
-  }
+    checkValidValidator(fieldName: string) {
+        return ((this.SymbolForm.controls[fieldName].dirty ||
+            this.SymbolForm.controls[fieldName].touched) &&
+            this.SymbolForm.controls[fieldName].invalid) ||
+            (this.isInvalidForm && this.SymbolForm.controls[fieldName].invalid)
+            ? 'ng-invalid ng-dirty'
+            : '';
+    }
 
-  checkValidMultiValidator(fieldNames: string[]) {
-      for (let i = 0; i < fieldNames.length; i++) {
-          if (
-              ((this.SymbolForm.controls[fieldNames[i]].dirty ||
-                  this.SymbolForm.controls[fieldNames[i]].touched) &&
-                  this.SymbolForm.controls[fieldNames[i]].invalid) ||
-              (this.isInvalidForm &&
-                  this.SymbolForm.controls[fieldNames[i]].invalid)
-          ) {
-              return true;
-          }
-      }
-      return false;
-  }
+    checkValidMultiValidator(fieldNames: string[]) {
+        for (let i = 0; i < fieldNames.length; i++) {
+            if (
+                ((this.SymbolForm.controls[fieldNames[i]].dirty ||
+                    this.SymbolForm.controls[fieldNames[i]].touched) &&
+                    this.SymbolForm.controls[fieldNames[i]].invalid) ||
+                (this.isInvalidForm &&
+                    this.SymbolForm.controls[fieldNames[i]].invalid)
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-  onSubmit() {
-      this.isSubmitted = true;
-      this.isInvalidForm = false;
-      if (this.SymbolForm.invalid) {
-          this.messageService.add({
-              severity: 'error',
-              detail: AppUtil.translate(
-                  this.translateService,
-                  'info.please_check_again'
-              ),
-          });
-          this.isInvalidForm = true;
-          this.isSubmitted = false;
-          return;
-      }
+    onSubmit() {
+        this.isSubmitted = true;
+        this.isInvalidForm = false;
+        if (this.SymbolForm.invalid) {
+            this.messageService.add({
+                severity: 'error',
+                detail: AppUtil.translate(
+                    this.translateService,
+                    'info.please_check_again'
+                ),
+            });
+            this.isInvalidForm = true;
+            this.isSubmitted = false;
+            return;
+        }
 
-      let newData = this.cleanObject(
-          AppUtil.cleanObject(this.SymbolForm.value)
-      );
-      console.log(newData);
-      this.onCancel.emit({});
-      if (this.isEdit) {
-          this.SymbolService
-              .updateSymbol(newData, this.formData.id)
-              .subscribe((res) => {
-                  this.onCancel.emit({});
-              });
-      } else {
-          this.SymbolService.createSymbol(newData).subscribe((res) => {
-              this.onCancel.emit({});
-          });
-      }
-  }
+        let newData = this.cleanObject(
+            AppUtil.cleanObject(this.SymbolForm.value)
+        );
+        this.onCancel.emit({});
+        if (this.isEdit) {
+            this.SymbolService.updateSymbol(
+                newData,
+                this.formData.id
+            ).subscribe((res) => {
+                this.onCancel.emit({});
+            });
+        } else {
+            this.SymbolService.createSymbol(newData).subscribe((res) => {
+                this.onCancel.emit({});
+            });
+        }
+    }
 
-  cleanObject(data) {
-      let newData = Object.assign({}, data);
-      if (!(newData.id > 0)) {
-          newData.id = 0;
-      }
-      return newData;
-  }
+    cleanObject(data) {
+        let newData = Object.assign({}, data);
+        if (!(newData.id > 0)) {
+            newData.id = 0;
+        }
+        return newData;
+    }
 
-  getDayOfWeek(date: any) {
-      return new Date(date.year, date.month, date.day).getDay();
-  }
+    onBack() {
+        this.onCancel.emit({});
+    }
 }
